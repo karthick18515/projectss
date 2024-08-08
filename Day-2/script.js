00b31df6dce46d8890a587849086299f4f7407c7
karthick <karthick18515>
2024-08-08 18:28:24 +0530

    projects

---
+++ Day-2/script.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+const para=<h1 >heading h1</h1>
+const butt=<button>submit</button>
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(butt)
