728fa695da1fb48da3184268cd94db9a24792417
karthick <karthick18515>
2024-08-08 19:43:52 +0530

    fg

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ const para=<p className="bg-black text-white">para</p>
+ const root=ReactDOM.createRoot(document.getElementById("root"))
+ root.render(para)
